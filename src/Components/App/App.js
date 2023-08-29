f0d78db54280c78118283269c6d7a158f2c4a9c4
Adam Meza <Adammeza95@gmail.com>
2023-08-29 10:35:13 -0400

    fix horizontal scroll

---
+++ src/Components/App/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import face from '../../assets/face.jpg'
+import { useScroll, motion, useTransform } from 'framer-motion';
+import { Carousel } from '../Carousel/Carousel';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <aside>
+        <div className='name-container'>
+          <h1>adam meza</h1>
+          <span className="creative">creative</span>
+        </div>
+        <span>code</span>
+        <span>ink</span>
+        <span>graphite</span>
+        <span>contact</span>
+      </aside> */}
+      <Carousel />  
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Carousel/Carousel.js
@@ -0,0 +1,30 @@
+import React, {useRef} from 'react'
+import './Carousel.css'
+import face from '../../assets/face.jpg'
+import shoe from '../../assets/2D-newBalance.jpg'
+import body from '../../assets/graphite-body1.jpg'
+import corona from '../../assets/graphite-corona.jpg'
+import { useScroll, motion, useTransform } from 'framer-motion';
+
+export const Carousel = () => {
+  const content = [face, body,corona, shoe].map(src => 
+                    <img className='carousel-item'
+                          src={src}/>
+                    )
+  
+  const targetRef = useRef(null)
+  const {scrollYProgress} = useScroll({
+    target: targetRef
+  }) 
+  
+  const x = useTransform(scrollYProgress, [0, 1], ["1%", "-50%"])
+  return (
+    <section ref={targetRef} className='carousel-wrapper'>
+     <div className='carousel-container'>
+      <motion.div style={{ x }} className='test'>
+        {content}
+      </motion.div>
+     </div>
+    </section>
+  )
+}
